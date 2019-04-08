101116881cd37688c34cfb7fee4517b1bd4f3ade
iigmir <roc120j@gmail.com>
2019-04-08 22:52:29 +0800

    init

---
+++ generate.sh
@@ -0,0 +1,11 @@
+# Create empty markdown by shell scripts
+
+for i in {1..45}
+    do
+        echo "" >> ./articles/$i.md
+    done
+
+for j in {78..88}
+    do
+        echo "" >> ./articles/$j.md
+    done
---
+++ write-old-mds.js
@@ -0,0 +1,23 @@
+const ary = require("./contents.json");
+const fs = require("fs");
+
+if( Array.isArray(ary) )
+{
+    ary.map( (item, index) =>
+    {
+        let filename = "";
+        if( item.hasOwnProperty("context") )
+        {
+            filename = `./articles/${ item.id }.md`;
+            fs.writeFile( filename , item.context , (err) =>
+            {
+                if (err) throw err;
+                console.log(`Write file: ./articles/${ item.id }.md`);
+            });
+        }
+    });
+}
+else
+{
+    console.log(ary);
+}
